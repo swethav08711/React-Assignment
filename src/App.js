c30583278a4c7623d757b009b7717eba4a65124b
swethav08711 <shwe0871@gmail.com>
2021-11-12 00:29:27 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react"
 import Counter from "./Counter/Counter"
 import { Task } from "./Input/Task"
 import { CssModel } from "./cssModel/CssModel"
-
+import { Cards } from "./ReactMobileOS/Cards"
 export default function App() {
   return (
     <div>
       <Counter />
       <Task />
       <CssModel />
+      <Cards />
     </div>
   )
 }
+
 //state is something that coimponent remembers
---
+++ src/ReactMobileOS/Cards.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+function Cards() {
+  return (
+    <>
+      <h1>Mobile Operating System</h1>
+      <ul>
+        <li>Andriod</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        <li>Samsung</li>
+        <li>Micromax</li>
+        <li>HTC</li>
+        <li>Apple</li>
+      </ul>
+    </>
+  )
+}
+export { Cards }
