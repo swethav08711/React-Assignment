9cc665ce31f52eb0042d90bf90cd8ab0b3daa68c
swethav08711 <shwe0871@gmail.com>
2021-11-27 11:31:35 +0530

    pagination

--- src/New/Todo.jsx
+++ src/New/Todo.jsx
@@ -5,7 +5,7 @@ import { v4 as uuid } from "uuid"
 import Pagination from "./pagination"
 function Todo() {
   const [list, setlist] = React.useState([])
-  const [showComplete, setShowCompleted] = useState(false)
+  // const [showComplete, setShowCompleted] = useState(false)
   const [page, setpage] = React.useState(1)
   const handleSubmit = ({ title, description }) => {
     const payload = {
@@ -16,13 +16,13 @@ function Todo() {
     }
     setlist([...list, payload])
   }
-  const changePageTo = num => {
-    if (num <= 1) {
-      setpage(1)
-      return
-    }
-    setpage(num)
-  }
+  //   const changePageTo = num => {
+  //     if (num <= 1) {
+  //       setpage(1)
+  //       return
+  //     }
+  //     setpage(num)
+  //   }
   const perpage = 2
   const filterResult = list.filter(
     (_, i) => i >= (page - 1) * perpage && i < page * perpage
